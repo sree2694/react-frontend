7d152930b0d56939fa564af57d1c845e983ee59e
sree2694 <58017164+sree2694@users.noreply.github.com>
2021-11-06 09:54:53 +0530

    Update FooterComponent.jsx

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -9,15 +9,15 @@ class FooterComponent extends Component {
         }
     }
 
-    render() {
-        return (
-            <div>
-                <footer className = "footer">
-                    <span className="text-muted">All Rights Reserved 2021@ Internship</span>
-                </footer>
-            </div>
-        )
-    }
+    // render() {
+    //     return (
+    //         <div>
+    //             <footer className = "footer">
+    //                 <span className="text-muted">All Rights Reserved 2021@ Internship</span>
+    //             </footer>
+    //         </div>
+    //     )
+    // }
 }
 
 export default FooterComponent
